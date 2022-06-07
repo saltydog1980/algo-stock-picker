9ca722cfb8f8de041337ba8b738e517076e04dba
saltydog1980 <mikeelambert99@gmail.com>
2022-06-06 23:41:47 -0400

    final code

--- js/stockPicker.js
+++ js/stockPicker.js
@@ -1,3 +1,18 @@
 exports.picker = function(prices) {
-
+    const dumpArray = [];
+    let count = 0
+    for (let i = 0; i < prices.length; i++) {
+        for (let j = i+1; j < prices.length; j++){
+            if (prices[i] >= prices[j]) {
+                continue
+            } else {
+                if (prices[j] - prices[i] > count) {
+                    count = prices[j] - prices[i]
+                    dumpArray.pop()
+                    dumpArray.push([i, j])
+                }
+            }
+        }
+    }
+    return dumpArray.flat()
 }
--- python/stock_picker.py
+++ python/stock_picker.py
@@ -1,2 +1,16 @@
 def picker(prices):
-    pass
+    ans_list = []
+    count = 0
+    for i, num in enumerate(prices):
+        for x in range(1, len(prices), 1):
+            if num > prices[x]:
+                pass
+            elif num < prices[x] and i < x:
+                if prices[x] - num > count:
+                    count = prices[x] - num
+                    if ans_list:
+                        ans_list.append([i,x])
+                        ans_list.pop(0)
+                    else:
+                        ans_list.append([i,x])
+    return ans_list[0]
